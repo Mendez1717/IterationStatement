af48db11454b8b1b0e3abd0b7c05ef18ac0ab633
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-02-19 21:06:59 -0500

    Changes made to IterationStatement to address integer.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace IterationStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Interation with For Statement: print 1-10 numbers onto the console
+            int i; // to count
+            int start = 1;
+            int end = 10;
+          
+          
+            int n;
+            Console.WriteLine("Enter Integer between 2 and 10: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 1)
+            {
+
+                int min = n - 1;
+                for (i = start; i <= min; i++)
+                {
+                    Console.WriteLine("You entered an odd number");
+                }
+            }
+            else
+            {
+                int max = n + 1;
+                for (i = start; i <= max; i++)
+                {
+                    Console.WriteLine("You entered an even number");
+                }
+            }
         }
     }
 }
